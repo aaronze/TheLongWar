e3e719e37c32f27670e4ffb9d6c4334fbb6b0d5f
Aaron Kison <aaron.kison@gmail.com>
2014-07-30 19:32:51 +0930

    Added feedback and changed server location

--- TheLongWarClient/src/secure/Network.java
+++ TheLongWarClient/src/secure/Network.java
@@ -1,6 +1,9 @@
 package secure;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.*;
 
 /**
@@ -9,7 +12,7 @@ import java.net.*;
 public class Network {
     private static Socket socket;
     
-    private final static String host = "localhost";
+    private final static String host = "192.168.0.99";
     private final static int port = 2574;
     
     /**
@@ -22,6 +25,17 @@ public class Network {
             public void run() {
                 try {
                     socket = new Socket(host, port);
+                    System.out.println("Connected!");
+                    
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+                    
+                    String userInput;
+                    while ((userInput = stdIn.readLine()) != null) {
+                        out.println(userInput);
+                        System.out.println("echo: " + in.readLine());
+                    }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
--- TheLongWarClient/src/ui/Home.java
+++ TheLongWarClient/src/ui/Home.java
@@ -1,10 +1,13 @@
 package ui;
 
 import javax.swing.JPanel;
+import secure.Network;
 
 /**
  * @author Aaron
  */
 public class Home extends JPanel {
-
+    public static void main(String[] args) {
+        Network.connect();
+    }
 }
