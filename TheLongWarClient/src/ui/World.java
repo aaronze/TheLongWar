feefd08c71c9c10ab1c2c3ca5d69448d464f13b1
Aaron Kison <aaron.kison@gmail.com>
2014-08-05 17:15:32 +0930

    Improved visual representation of countries

--- TheLongWarClient/src/ui/World.java
+++ TheLongWarClient/src/ui/World.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.MouseInfo;
 import java.awt.Point;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import java.awt.image.BufferedImage;
@@ -83,11 +85,14 @@ public class World extends JPanel {
     }
     
     private BufferedImage worldImage;
+    private BufferedImage mapOverlay;
     private String selectedCountry = "";
     
     public World() {
         try {
             worldImage = ImageIO.read(Resources.getResource("world.jpg"));
+            mapOverlay = new BufferedImage(worldImage.getWidth(), worldImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
+            buildOverlay();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -98,14 +103,70 @@ public class World extends JPanel {
                 double mx = e.getX() * 1.0 / getWidth();
                 double my = e.getY() * 1.0 / getHeight();
                 
+                String previous = selectedCountry;
                 selectedCountry = getCountryAt(mx, my);
+                
+                if (!previous.equals(selectedCountry)) {
+                    buildOverlay();
+                }
             }
         });
+        
+        addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                buildOverlay();
+            }
+        });
+    }
+    
+    public void buildOverlay() {
+        if (this.getWidth() != 0) {
+            mapOverlay.getGraphics().drawImage(
+                        worldImage.getScaledInstance(this.getWidth(), this.getHeight(), BufferedImage.SCALE_DEFAULT), 0, 0, null);
+
+            double scaleX = this.getWidth() * 1.0 / worldImage.getWidth();
+            double scaleY = this.getHeight() * 1.0 / worldImage.getHeight();
+
+            if (!selectedCountry.isEmpty()) {
+                double stepX = 1.0 / mapOverlay.getWidth();
+                double stepY = 1.0 / mapOverlay.getHeight();
+
+                for (int x = 0; x < mapOverlay.getWidth(); x++) {
+                    for (int y = 0; y < mapOverlay.getHeight(); y++) {
+                        String country = getCountryAt(x * stepX, y * stepY);
+
+                        int xP = (int)(x * scaleX);
+                        int yP = (int)(y * scaleY);
+
+                        if (country.equals(selectedCountry)) {
+                            mapOverlay.setRGB(xP, yP, increaseBrightness(mapOverlay.getRGB(xP, yP), 40));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    public int increaseBrightness(int color, int amount) {
+        int r = (color >> 16) & 0xFF;
+        int g = (color >> 8) & 0xFF;
+        int b = color & 0xFF;
+        
+        r += amount;
+        g += amount;
+        b += amount;
+        
+        r = r > 255 ? 255 : r;
+        g = g > 255 ? 255 : g;
+        b = b > 255 ? 255 : b;
+        
+        return (255 << 24) | (r << 16) | (g << 8) | b;
     }
 
     @Override
     public void paint(Graphics g) {
-        g.drawImage(worldImage.getScaledInstance(this.getWidth(), this.getHeight(), BufferedImage.SCALE_DEFAULT), 0, 0, null);
+        g.drawImage(mapOverlay, 0, 0, null);
         
         if (!selectedCountry.isEmpty()) {
             Point mouse = MouseInfo.getPointerInfo().getLocation();
