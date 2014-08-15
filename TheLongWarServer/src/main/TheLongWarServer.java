8c272802a9e0dfe4e60a7abc6dad3efb0bc51d46
Aaron Kison <aaron.kison@gmail.com>
2014-08-15 15:43:41 +0930

    Removed test code

--- TheLongWarServer/src/main/TheLongWarServer.java
+++ TheLongWarServer/src/main/TheLongWarServer.java
@@ -13,19 +13,6 @@ public class TheLongWarServer {
     private static final int port = 2574;
     
     public static void main(String[] args) {
-        Army aggressor = new Army("Attacker");
-        Army defender = new Army("Defender");
-        
-        for (int i = 0; i < 100; i++) {
-            aggressor.add(new Unit("Archer"));
-        }
-        for (int i = 0; i < 100; i++) {
-            defender.add(new Unit("Warrior"));
-        }
-        
-        System.out.println(Simulator.simulateBattle(aggressor, defender));
-        System.out.println(Simulator.simulateBattle(aggressor, defender));
-        
         try {
             ServerSocket ss = new ServerSocket(port);
             
